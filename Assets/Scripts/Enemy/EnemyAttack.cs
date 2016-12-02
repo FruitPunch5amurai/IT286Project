0cadf1dd2253699f6b95a057093179bb1cad7e7c
SirRavager <flamenbst@gmail.com>
2016-12-02 11:41:09 -0500

    Merged branches

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class EnemyAttack : MonoBehaviour {
+
+    public float AttackInterval;
+
+    protected EnemyAI m_EnemyAI;
+    protected float m_timeUntilCanAttack;
+    protected Vector3 m_dir;
+    protected RaycastHit2D ray;
+    protected bool m_hit;
+    protected int m_ingnoreLayer = 1 << 8;
+    public GameObject Bullet;
+    public float BulletSpeed;
+    public GameObject Target;
+    // Use this for initialization
+    protected virtual void Start () {
+        m_EnemyAI = GetComponent<EnemyAI>();
+        m_ingnoreLayer = ~m_ingnoreLayer;
+    }
+
+    // Update is called once per frame
+    protected virtual void FixedUpdate() {
+        
+	}
+    
+}
---
+++ Assets/Scripts/RoomCollider.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomCollider : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //This handles detecting what room the player is in
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "RoomDoor" && transform.parent.tag == "Player")
+        {
+            GameManager.singleton.TransitionRoom(col.transform.parent.GetComponent<DunGen.Doorway>().ConnectedDoorway.transform.parent.gameObject,
+                col.transform.parent.GetComponent<DunGen.Doorway>().ConnectedDoorway.transform.GetChild(1).position);
+        }
+        else if(col.tag == "Room")
+        {
+            if (transform.parent.tag == "Player")
+            {
+                transform.parent.GetComponent<PlayerControl>().CurrentRoom = col.gameObject;
+            }
+            else if(transform.parent.tag == "Enemy")
+            {
+                transform.parent.GetComponent<EnemyAI>().CurrentRoom = col.gameObject;
+            }
+        }
+    }
+}
